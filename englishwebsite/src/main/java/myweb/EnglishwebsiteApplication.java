2cbcfa812781b742182d7f04d512fe20b57621b7
thanhgiau2604 <thanhgiau2604@gmail.com>
2019-05-09 22:56:07 +0700

    up

--- englishwebsite/src/main/java/myweb/EnglishwebsiteApplication.java
+++ englishwebsite/src/main/java/myweb/EnglishwebsiteApplication.java
@@ -2,11 +2,12 @@ package myweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
-public class EnglishwebsiteApplication {
+public class EnglishwebsiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnglishwebsiteApplication.class, args);
